ca57727639996a873dcda56c8344df55e3b7868f
Dom Williams <me@domwillia.ms>
2017-03-31 10:46:02 +0200

    Ajout d'interface en ligne de commande

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,20 +1,221 @@
+#include <cstdio>
 #include <iostream>
+#include <fstream>
+#include <getopt.h>
 #include "Printer.h"
 #include "Element.h"
 
 extern int yyparse(Document *);
+extern FILE *yyin;
+
+struct Settings
+{
+	Settings() :
+		staticAnalysis(false), optimisation(false),
+		compile(false), compileOutFileSpecified(false),
+		astTree(false), astOutFileSpecified(false) {}
+
+	void printSelf() const
+	{
+		std::cout << "Compiling file '" << fileToCompile << "'";
+
+		if (staticAnalysis)
+			std::cout << ", static analysis";
+
+		if (optimisation)
+			std::cout << ", optimising";
+
+		if (compile)
+		{
+			std::cout << ", compiling to ";
+			if (compileOutFileSpecified)
+				std::cout << "specified file '" << compileOutFile << "'";
+			else
+				std::cout << "default .asm file";
+		}
+
+		if (astTree)
+		{
+			std::cout << ", printing AST to ";
+			if (astOutFileSpecified)
+				std::cout << "specified file '" << astOutFile << "'";
+			else
+				std::cout << "default .png file";
+		}
+
+	};
+
+	bool staticAnalysis;
+	bool optimisation;
+	bool compile;
+	bool compileOutFileSpecified;
+	std::string compileOutFile;
+
+	bool astTree;
+	bool astOutFileSpecified;
+	std::string astOutFile;
+
+	std::string fileToCompile;
+};
+
+int doWork(const Settings &settings);
 
 int main(int argc, char **argv)
 {
+	// ./c <fichier>
+	// -a -> static analysis
+	// -o -> optimisation
+	// -c -> compile to <filename>.asm
+	// --outfile <filename> -> defaults to filename.asm
+	// -t -> ast.png
+	// --astfile -> output file for ast.png
+
+	Settings settings;
+
+	option options[] = {
+		{"outfile", required_argument, 0, 0},
+		{"astfile", required_argument, 0, 0},
+		{0, 0, 0, 0}
+	};
+
+	int optionIndex = 0;
+
+	int i;
+	while ((i = getopt_long(argc, argv, "aoct", options, &optionIndex)) != -1)
+	{
+		switch (i)
+		{
+			// long option
+			case 0:
+				switch (optionIndex) // god help us all
+				{
+					case 0:
+						settings.compileOutFile = optarg;
+						settings.compileOutFileSpecified = true;
+						break;
+
+					case 1:
+						settings.astOutFile = optarg;
+						settings.astOutFileSpecified = true;
+						break;
+				}
+				break;
+
+			case 'a':
+				settings.staticAnalysis = true;
+				break;
+
+			case 'c':
+				settings.compile = true;
+				break;
+
+			case 'o':
+				settings.optimisation = true;
+				break;
+
+			case 't':
+				settings.astTree = true;
+				break;
+
+		}
+	}
+
+	if (optind + 1 != argc)
+	{
+		std::cerr << "A single file to compile must be given" << std::endl;
+		return 1;
+	}
+
+	settings.fileToCompile = argv[optind];
+
+	return doWork(settings);
+}
+
+void parseFileName(const std::string &originalFile, const std::string &replacement, const std::string &extension, std::ostream **out,
+		bool &freeMe)
+{
+	*out = NULL;
+
+	// stdout
+	if (replacement == "-")
+	{
+		*out = &std::cout;
+		return;
+	}
+
+	std::string outFile(replacement);
+	if (replacement.empty())
+	{
+		// get prefix
+		size_t dotIndex = originalFile.rfind('.');
+		if (dotIndex == std::string::npos)
+			dotIndex = originalFile.size();
+
+		outFile = std::string(originalFile, 0, dotIndex);
+		outFile += extension;
+	}
+
+	*out = new std::ofstream(outFile);
+	freeMe = true;
+}
+
+int doWork(const Settings &settings)
+{
+	// open file
+	FILE *in = std::fopen(settings.fileToCompile.c_str(), "r");
+	if (in == NULL)
+	{
+		std::cerr << "Failed to open file '" << settings.fileToCompile << "'" << std::endl;
+		return 1;
+	}
+	yyin = in;
+
+	// create AST
 	Document d;
 	int ret = yyparse(&d);
+	fclose(in);
 	if (ret != 0)
-		return 1;
-
+		return 2;
 	d.createBlocks();
 
-	GraphPrinter printer(std::cerr);
-	printer.printGraph(&d);
+	// print ast
+	if (settings.astTree)
+	{
+		std::ostream *os;
+		bool freeMe = false;
+		parseFileName(settings.fileToCompile, settings.astOutFile, ".dot", &os, freeMe);
+		if (os == NULL)
+		{
+			std::cerr << "Invalid ast out file" << std::endl;
+			return 3;
+		}
 
-	std::cout << std::endl;
+		GraphPrinter printer(*os);
+		printer.printGraph(&d);
+		*os << std::endl;
+
+		if (freeMe)
+			delete os;
+	}
+
+	// TODO optimise
+
+	if (settings.compile)
+	{
+		std::ostream *os;
+		bool freeMe = false;
+		parseFileName(settings.fileToCompile, settings.compileOutFile, ".o", &os, freeMe);
+		if (os == NULL)
+		{
+			std::cerr << "Invalid compile out file" << std::endl;
+			return 4;
+		}
+
+		// TODO compile me
+
+		if (freeMe)
+			delete os;
+	}
+
+	return 0;
 }
